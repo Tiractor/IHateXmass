a43233c3d594bb18993a4e296654b26f7593f196
Рената Салихова <90962760+IRenataI@users.noreply.github.com>
2022-12-19 20:16:03 +0300

    MoreFixes

--- Score23/Assets/EnterGame.cs
+++ Score23/Assets/EnterGame.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class EnterGame : MonoBehaviour
 {
     public GameObject Wall;
+    public GameObject Spawner;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Wall.SetActive(true);
+            Spawner.SetActive(true);
             Destroy(gameObject);
         }
     }
