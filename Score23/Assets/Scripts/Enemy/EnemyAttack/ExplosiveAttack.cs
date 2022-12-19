45f402349168212b404bc49cb70aac8c88a875ab
Tiractor <tiractorgamer@gmail.com>
2022-12-19 14:46:18 +0300

    Repair: ExplosiveAttack

--- Score23/Assets/Scripts/Enemy/EnemyAttack/ExplosiveAttack.cs
+++ Score23/Assets/Scripts/Enemy/EnemyAttack/ExplosiveAttack.cs
@@ -5,15 +5,8 @@ public class ExplosiveAttack : AttackType
     [SerializeField] private GameObject _explosionEffect;
     public override Unit ReturnTarget()
     {
-
-        _explosionEffect.transform.SetParent(null);
-        _explosionEffect.SetActive(true);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), transform.forward, out hit, Range))
-        {
-            var unit = hit.transform.GetComponent<Unit>();
-            unit.ApplyDMG(gameObject.GetComponent<ExplosiveAttack>());
-        }
-        return gameObject.GetComponent<Unit>();
+        Instantiate(_explosionEffect, transform.position, Quaternion.identity, transform);
+        gameObject.GetComponent<Unit>().ApplyDMG(gameObject.GetComponent<ExplosiveAttack>());
+        return GameObject.FindWithTag("Player").GetComponent<Unit>();
     }
 }
